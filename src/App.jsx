8e7330a82e1f1250b54fb36f58acb16b1e7c7d4b
Piyush Kochar <piyushkochar909@gmail.com>
2025-07-06 21:03:04 +0530

    added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,30 @@ import Header from "./components/header";
 import FileDisplay from "./components/filedisplay";
 import { useState } from "react";
 
-function handleResetAudio(){
-  setFile(null);
-  setAudio(null);
-}
-
 function App() {
-  const [file , setFile] = useState(null)
-  const [audio , setAudio] = useState(null)
-  const isAudioPresent = file || audio
+  const [file, setFile] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const isAudioPresent = file || audio;
+
+  function handleResetAudio() {
+    setFile(null);
+    setAudio(null);
+  }
+
   return (
-  <div className="flex flex-col max-w-[1000] mx-auto w-full">
-    <section className="min-h-screen flex flex-col">
-     <Header />
-     {isAudioPresent ? (<FileDisplay file={file} audio={audio} reset={handleResetAudio}/>) : (<Homepage setFile={setFile} setAudio={setAudio}/>)}
-    </section>
-    <h1>
-      hello
-    </h1>
-    <footer>
-
-    </footer>
-  </div>
-
-  )
+    <div className="flex flex-col max-w-[1000] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        {isAudioPresent ? (
+          <FileDisplay file={file} audio={audio} reset={handleResetAudio} />
+        ) : (
+          <Homepage setFile={setFile} setAudio={setAudio} />
+        )}
+      </section>
+      <h1>hello</h1>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
--- src/components/filedisplay.jsx
+++ src/components/filedisplay.jsx
@@ -1,13 +1,24 @@
-
-
-export default function FileDisplay({reset , file , audio}) {
+export default function FileDisplay({ reset, file, audio }) {
   return (
-    <div>
-    <main className="flex flex-1 flex-col gap-3 sm:gap-4 md:gap-5 p-4 text-center justify-center">
-        <h1 className="font-black text-4xl sm:text-5xl md:text-6xl">
-        Your<span className="text-pink-700 ">File</span>
+    <main className="flex flex-1 flex-col gap-3 sm:gap-4 md:gap-5 p-4 text-center justify-center w-fit mx-auto max-w-full">
+      <h1 className="font-black text-4xl sm:text-5xl md:text-6xl">
+        Your<span className="text-pink-700 "> File</span>
       </h1>
+      <div className="flex item-center gap-2 mx-auto  flex-col text-left my-7">
+        <h3 className="font-extrabold">Name</h3>
+        <p>{file.name}</p>
+      </div>
+      <div className="flex items-center justify-evenly gap-4">
+        <button
+          className="text-slate-400 hover:text-gray-900 duration-200"
+          onClick={reset}
+        >
+          Reset
+        </button>
+        <button className="specialBtn px-4 py-3 rounded-2xl text-pink-500 hover:text-pink-800 duration-150">
+          Transcribe
+        </button>
+      </div>
     </main>
-    </div>
-  )
+  );
 }
