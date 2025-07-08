5e0511ba9f1dad1edbfee27e25bcfac3b42b3ea2
Piyush Kochar <piyushkochar909@gmail.com>
2025-07-08 22:05:31 +0530

    installed xenova dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Result from "./components/result";
 import Loader from "./components/loader";
 import { useState, useEffect, useRef } from "react";
 import { MessageTypes } from "./presets";
+import {pipeline} from '@huggingface/transformers'
+
 
 function App() {
   const [file, setFile] = useState(null);
@@ -12,40 +14,45 @@ function App() {
   const [output, setOutput] = useState(null);
   const [loading, setLoading] = useState(false);
 
-
-
+ 
   function handleResetAudio() {
     setFile(null);
     setAudio(null);
   }
 
   useEffect(() => {
-    console.log("audio fetched")
+    console.log("audio fetched");
     console.log(audio);
-  }, [audio])
 
+  }, [audio]);
 
   const isAudioPresent = file || audio;
+
+
+
+
 
   return (
     <div className="flex flex-col max-w-[1000] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-       {(() => {
-  if (output) {
-    return <Result />;
-  }
+        {(() => {
+          if (output) {
+            return <Result />;
+          }
 
-  if (loading) {
-    return <Loader isLoading={loading} />;
-  }
+          if (loading) {
+            return <Loader isLoading={loading} />;
+          }
 
-  if (isAudioPresent) {
-    return <Display file={file} audio={audio} reset={handleResetAudio} />;
-  }
+          if (isAudioPresent) {
+            return (
+              <Display file={file} audio={audio} reset={handleResetAudio} />
+            );
+          }
 
-  return <Homepage setFile={setFile} setAudio={setAudio} />;
-})()}
+          return <Homepage setFile={setFile} setAudio={setAudio} />;
+        })()}
       </section>
 
       <footer></footer>
