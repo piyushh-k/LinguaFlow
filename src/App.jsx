1c94d48103a69d51f6dedfb62c28900710f92fbf
Piyush Kochar <piyushkochar909@gmail.com>
2025-07-08 18:53:43 +0530

    fixed ternary logic

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { MessageTypes } from "./presets";
 function App() {
   const [file, setFile] = useState(null);
   const [audio, setAudio] = useState(null);
-  const [output, setOutput] = useState(2);
+  const [output, setOutput] = useState(null);
   const [loading, setLoading] = useState(false);
   const [downloading, setDownloading] = useState(false);
   const [finished, setFinished] = useState(false);
@@ -56,22 +56,22 @@ function App() {
   });
 
   async function readAudio(file) {
-    const audioCTX = new AudioContext({sampleRate : 16000})
+    const audioCTX = new AudioContext({ sampleRate: 16000 });
     const response = await file.arrayBuffer();
-    const decoded = await audioCTX.decodeAudioData(response)
+    const decoded = await audioCTX.decodeAudioData(response);
     const audio = decoded.getChannelData(0);
     return audio;
   }
 
   async function handleSubmit() {
-    if(!file && !audio)return;
+    if (!file && !audio) return;
 
     let voice = await readAudio(file ? file : audio);
-    const model_name = 'openAI/whisper-tiny.en';
+    const model_name = "openAI/whisper-tiny.en";
 
     worker.current.postMessage({
-      type : MessageTypes.INFERENCE_REQUEST,
-    })
+      type: MessageTypes.INFERENCE_REQUEST,
+    });
   }
 
   const isAudioPresent = file || audio;
@@ -80,15 +80,23 @@ function App() {
     <div className="flex flex-col max-w-[1000] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {output ? (
-          <Result />
-        ) : loading ? (
-          <Loader isLoading={loading} />
-        ) : isAudioPresent ? (
-          <Display file={file} audio={audio} reset={handleResetAudio} />
-        ) : (
-          <Homepage setFile={setFile} setAudio={setAudio} />
-        )}
+        {(() => {
+          if (output) {
+            return <Result />;
+          }
+
+          if (loading) {
+            return <Loader isLoading={loading} />;
+          }
+
+          if (isAudioPresent) {
+            return (
+              <Display file={file} audio={audio} reset={handleResetAudio} />
+            );
+          }
+
+          return <Homepage setFile={setFile} setAudio={setAudio} />;
+        })()}
       </section>
 
       <footer></footer>
